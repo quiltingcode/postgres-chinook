490ffdb432f8120f2294879fa0d3a642dd4cff46
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-12-02 12:33:34 +0000

    Install SQLAlchemy

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -3,7 +3,19 @@ connection = psycopg2.connect(database="chinook")
 
 cursor = connection.cursor()
 
+# cursor.execute('SELECT * FROM "Artist"')
+# cursor.execute('SELECT "Name" FROM "Artist"')
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', ["51"])
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', ["51"])
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["AC/DC"])
+
 results = cursor.fetchall()
 
 # results = cursor.fetchone()
 
+connection.close()
+
+for result in results:
+    print(result)
